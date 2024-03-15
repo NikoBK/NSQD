6a71e1aa8cc6f3027a3355a2170b40f0c591a3da
NikoBK <nkrebs21@student.aau.dk>
2024-03-15 15:00:01 +0100

    new setup

--- src/drone.cpp
+++ src/drone.cpp
@@ -3,4 +3,14 @@
  * created: mar 15 2024
 */
 
+#include "../include/config.hpp"
+#include "../include/camera.hpp"
 
+int main() {
+		loadConfig();
+		return 0;
+}
+
+void loadConfig() {
+		// TODO: Implement something here...
+}
