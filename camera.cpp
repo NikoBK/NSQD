a3c0cbddf7019e784fd958c0831242b47981aec2
NikoBK <nkrebs21@student.aau.dk>
2024-03-15 11:08:08 +0100

    create camera code files

---
+++ camera.cpp
@@ -0,0 +1,16 @@
+/* camera.cpp
+ * author: nikobk
+ * created: mar 15 2024
+*/
+
+#include <iostream>
+#include <string>
+
+int main() {
+		initCam();
+		return 0;
+}
+
+void initCam() {
+		// TODO: Implement something here.
+}
---
+++ camera.hpp
@@ -0,0 +1,11 @@
+/* camera.hpp
+ * author: nikobk
+ * created: mar 15 2024
+*/
+
+#ifndef CAMERA_HPP
+#define CAMERA_HPP
+
+void initCam();
+
+#endif
